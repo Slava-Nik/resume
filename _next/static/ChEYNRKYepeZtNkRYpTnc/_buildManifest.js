f872d8f74f91e2eec829e36aa67812f081a6ffd6
Slava-Nik <lancerokey@yandex.ru>
2023-01-25 20:45:44 +0000

    Deploying to gh-pages from @ Slava-Nik/resume@d6ad7441b906148ab405783b0d5fc6a8c4002df6 🚀

---
+++ _next/static/ChEYNRKYepeZtNkRYpTnc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b044c280d6f88702.css","static/chunks/pages/index-a8e3e4bb93f2d910.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
